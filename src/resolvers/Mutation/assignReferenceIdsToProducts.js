b4313bb07063e215fcebae1144483165d4b5937b
RehanMaroof <rehan.maroof@codistan.org>
2023-10-05 17:38:26 +0500

    update: filter for referenceId

--- src/resolvers/Mutation/assignReferenceIdsToProducts.js
+++ src/resolvers/Mutation/assignReferenceIdsToProducts.js
@@ -1,16 +1,19 @@
 export default async function assignReferenceIdsToProducts(_, { input }, context) {
   const { collections } = context;
-const { Products } = collections;
+  const { Products } = collections;
 
-// Define a filter for products that don't have a referenceId
-const filter = { referenceId: { $exists: false } };
+  // Define a filter for products that don't have a referenceId
+  const filter = { referenceId: { $exists: false } };
 
-// Generate a random referenceId
-const randomReferenceId = Math.floor(Math.random() * 1000000).toString();
+  // Find all products that don't have a referenceId
+  const productsWithoutReferenceId = await Products.find(filter).toArray();
 
-// Update all products that don't have a referenceId
-await Products.updateMany(filter, { $set: { referenceId: randomReferenceId } });
+  // Loop over the products and assign a unique referenceId to each one
+  for (const product of productsWithoutReferenceId) {
+    // Generate a random referenceId
+    const randomReferenceId = Math.floor(Math.random() * 1000000).toString();
 
-  
+    // Update the product with the new referenceId
+    await Products.updateOne({ _id: product._id }, { $set: { referenceId: randomReferenceId } });
   }
-  
+}
