39951fed8802ce053a37dba26edeb94edcc6057c
shoaibfarooq3 <shoaibfarooq3@gmail.com>
2023-10-31 14:26:13 +0500

    Version updation

--- src/resolvers/Query/index.js
+++ src/resolvers/Query/index.js
@@ -1,8 +1,8 @@
 import product from "./product.js";
 import products from "./products.js";
-import optionTitle from "./updateOptionTitle.js";
+import updateOptionTitle from "./updateOptionTitle.js";
 export default {
   product,
   products,
-  optionTitle,
+  updateOptionTitle,
 };
